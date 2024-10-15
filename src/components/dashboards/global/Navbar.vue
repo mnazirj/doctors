bb22d9c259fc839ae15060c01ca189cacd1ec942
Mohammad-Alqahf <abosaber620@gmail.com>
2024-10-15 10:12:57 +0300

    Create Sidebar & design it  and design Navbar and Design cards in HomeView

--- src/components/dashboards/global/Navbar.vue
+++ src/components/dashboards/global/Navbar.vue
@@ -1,3 +1,52 @@
 <template>
-  <h1>Nav</h1>
+  <nav
+    class="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-between align-content-between"
+  >
+    <div class="d-flex justify-content-center ms-4">
+      <a class="navbar-brand" href="#"
+        ><img
+          src="../../../assets/images/homepage/logo.svg"
+          alt="logo"
+          class="brand-icon"
+      /></a>
+    </div>
+    <div class="w-16 d-flex justify-content-center me-2">
+        <div class=" flex justify-content-center align-items-center me-3 notification">
+            <i class="pi pi-bell"></i>
+            <div class="notification-number">1</div>
+        </div>
+      <div class="dropdown">
+        <a
+          class="nav-link dropdown-toggle nav-image-user"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <!-- <i class="pi pi-user text-danger rounded-circle nav-icon-user"></i> -->
+          <img
+            src="../../../assets/images/homepage/avatar-03.bcd54570.jpg"
+            alt="user-image"
+            class="rounded-circle w-100 h-100"
+          />
+        </a>
+        <ul class="dropdown-menu">
+          <li class="dropdown-item">
+            <a class="" href="#"
+              ><i class="pi pi-user me-2"></i><span>Profile</span></a
+            >
+          </li>
+          <li class="dropdown-item">
+            <a class="" href="#"
+              ><i class="pi pi-cog me-2"></i><span>Settings</span></a
+            >
+          </li>
+          <li class="dropdown-item">
+            <a class="" href="#"
+              ><i class="pi pi-sign-out me-2"></i><span>Logout</span></a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
---
+++ src/components/dashboards/global/Sidebar.vue
@@ -0,0 +1,102 @@
+<template>
+  <aside
+    class="h-full ps-3 sidebarr row" ref="sidebarr" id="sidebarr"
+  >
+    <div class="col" id="x">
+      <ul class="pl-0 pe-0 d-flex flex-wrap flex-column sidebar-menu">
+        <li class=" rounded rounded-3 sidebar-item active">
+          <i class="pi pi-home" title="Home"></i><span>Home</span>
+        </li>
+        <li class="rounded rounded-3 sidebar-item">
+          <i class="pi pi-calendar" title="Appointments"></i
+          ><span>Appointments</span>
+        </li>
+        <li class=" rounded rounded-3 sidebar-item">
+          <i class="pi pi-sparkles" title="Specialities"></i><span>Specialities</span>
+        </li>
+        <li class="  rounded rounded-3 sidebar-item">
+          <i class="pi pi-users" title="Doctors"></i><span>Doctors</span>
+        </li>
+        <li class="  rounded rounded-3 sidebar-item">
+          <i class="pi pi-user" title="Patients"></i><span>Patients</span>
+        </li>
+        <li class="rounded rounded-3 sidebar-item">
+          <i class="pi pi-star-fill" title="Reviews"></i><span>Reviews</span>
+        </li>
+        <li class=" rounded rounded-3 sidebar-item">
+          <i class="pi pi-cog" title="Websit Settings"></i><span>Websit Settings</span>
+        </li>
+      </ul>
+    </div>
+
+    <div
+      class="h-full bg-dark col-1 d-flex justify-content-center align-items-center flex-column toggle-btn"
+    >
+      <button
+        class="p-0 m-0 w-auto h-auto btn border-none"
+        @click="SidebarToggle"
+      >
+        <i class="pi pi-angle-right text-light" ref="toggleBtn"></i>
+      </button>
+    </div>
+  </aside>
+
+  <!-- <aside id="sidebar">
+    <div class="d-flex">
+      <button class="toggle-btn" type="button" ref="toggleBtn" @click="toggle">
+        <i class="pi pi-th-large"></i>
+      </button>
+      <div class="sidebar-logo">
+        <a href="">Sidebar</a>
+      </div>
+    </div>
+    <ul class="sidebar-nav">
+      <li class="sidebar-item">
+        <a><i class="pi pi-home p-1 m-1"></i><span>Home</span></a>
+      </li>
+      <li class="sidebar-item">
+        <a href="" class="sidebar-link"
+          ><i class="pi pi-calendar p-1 m-1"></i><span>Appointments</span></a
+        >
+      </li>
+      <li class="sidebar-item">
+        <a href="" class="sidebar-link"
+          ><i class="pi pi-sparkles p-1 m-1"></i><span>Specialities</span></a
+        >
+      </li>
+      <li class="sidebar-item">
+        <a href="" class="sidebar-link"
+          ><i class="pi pi-users p-1 m-1"></i><span>Doctors</span></a
+        >
+      </li>
+      <li class="sidebar-item">
+        <a href="" class="sidebar-link"
+          ><i class="pi pi-star-fill p-1 m-1"></i><span>Reviews</span></a
+        >
+      </li>
+      <li class="sidebar-item">
+        <a href="" class="sidebar-link"
+          ><i class="pi pi-cog p-1 m-1"></i><span>Websit Settings</span></a
+        >
+      </li>
+    </ul>
+  </aside> -->
+</template>
+
+<script>
+export default {
+  methods:{
+    SidebarToggle(){
+      // this.$refs.sidebarr.classList.toggle('expand');
+      // console.log(this.$refs.sidebarr.classList.toggle('expand'));
+      if(this.$refs.sidebarr.classList.toggle('expand')){
+        this.$refs.toggleBtn.classList.remove('pi-angle-right');
+        this.$refs.toggleBtn.classList.add('pi-angle-left');
+      }else{
+        this.$refs.toggleBtn.classList.add('pi-angle-right');
+        this.$refs.toggleBtn.classList.remove('pi-angle-left');
+      }
+    }
+  }
+};
+</script>
